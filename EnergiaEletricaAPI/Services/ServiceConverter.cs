a231625a8c345f5e15c7430d3a5fc101f9ae1b55
Thomaz Henrique <treimberg7@gmail.com>
2021-04-07 22:39:13 -0300

    Adicionando o gitignore

---
+++ EnergiaEletricaAPI/Services/ServiceConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergiaEletricaAPI.Services
+{
+    public class ServiceConverter
+    {
+        public void GenerateFileConverted ()
+        {
+
+        }
+
+    }
+}
